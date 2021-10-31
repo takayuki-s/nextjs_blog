2717b9c4926b1f71d32da9560f291e87c5ac903e
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-11-01 07:22:57 +0900

    タグ一覧表示

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -11,7 +11,33 @@ const PostDetail: React.FC<POST> = ({
   tags,
   created_at,
 }) => {
-  return <Layout title={title}></Layout>
+  return (
+    <Layout title={title}>
+      <div>
+        {tags &&
+          tags.map((tag, i) => (
+            <span
+              className={`px-2 py-2 m-1 text-white rounded ${
+                i === 0
+                  ? 'bg-blue-500'
+                  : i === 1
+                  ? 'bg-gray-500'
+                  : i === 2
+                  ? 'bg-green-500'
+                  : i === 3
+                  ? 'bg-yellow-500'
+                  : i === 4
+                  ? 'bg-indigo-500'
+                  : 'bg-gray-400'
+              }`}
+              key={tag.id}
+            >
+              {tag.name}
+            </span>
+          ))}
+      </div>
+    </Layout>
+  )
 }
 
 export default PostDetail
