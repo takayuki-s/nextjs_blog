e1eae0cf70f3f13e2f587808dd8ad4887fcddeb7
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-10-31 09:14:36 +0900

    ログアウトボタン設置

--- pages/index.tsx
+++ pages/index.tsx
@@ -67,6 +67,24 @@ const BlogPage: React.FC<STATICPROPS> = ({ posts }) => {
             </li>
           ))}
       </ul>
+      {hasToken && (
+        <svg
+          onClick={logout}
+          data-testid="logout-icon"
+          className="w-6 h-6 mt-10 cursor-pointer"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
+          ></path>
+        </svg>
+      )}
     </Layout>
   )
 }
