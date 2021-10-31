bc024b66a1f32096148ccbd6490cec29ad372bf3
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-11-01 07:30:16 +0900

    戻るボタン追加

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -42,6 +42,25 @@ const PostDetail: React.FC<POST> = ({
       <p className="mt-3">
         {'by '} {username}
       </p>
+      <Link href="/">
+        <div className="flex cursour-pointer mt-12">
+          <svg
+            className="w-6 h-6 mr-3"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+            ></path>
+          </svg>
+          <a data-testid="back-blog">Back to blog page</a>
+        </div>
+      </Link>
     </Layout>
   )
 }
