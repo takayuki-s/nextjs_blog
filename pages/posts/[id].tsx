abbf04a0219fc7ac2150fd1139253b4c2e94ae9b
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-11-02 05:59:45 +0900

    投稿が表示されない不具合を解消

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -76,7 +76,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
 }
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  const { post: post } = await getPostData(ctx.params.id as string)
+  const post = await getPostData(ctx.params.id as string)
   return {
     props: {
       ...post,
