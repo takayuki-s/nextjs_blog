0b18376dde9d310aefbc02c36725eace6cfe52c6
takayuki-s <69967620+takayuki-s@users.noreply.github.com>
2021-11-04 06:30:52 +0900

    モックしたAPIのレスポンス

--- __tests__/AuthPage.test.tsx
+++ __tests__/AuthPage.test.tsx
@@ -8,3 +8,50 @@ import { initTestHelpers } from 'next-page-tester'
 
 initTestHelpers()
 process.env.NEXT_PUBLIC_RESTAPI_URL = 'http://127.0.0.1:8000/api'
+
+const handlers = [
+  rest.post(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/jwt/create/`,
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json({ access: '123xyz' }))
+    }
+  ),
+  rest.post(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/register/`,
+    (req, res, ctx) => {
+      return res(ctx.status(201))
+    }
+  ),
+  rest.get(
+    `${process.env.NEXT_PUBLIC_RESTAPI_URL}/get-blogs/`,
+    (req, res, ctx) => {
+      return res(
+        ctx.status(200),
+        ctx.json([
+          {
+            id: 1,
+            title: 'title',
+            content: 'content1',
+            username: 'username1',
+            tags: [
+              { id: 1, name: 'tag1' },
+              { id: 2, name: 'tag2' },
+            ],
+            created_at: '2021-01-12 14:59:41',
+          },
+          {
+            id: 2,
+            title: 'title',
+            content: 'content2',
+            username: 'username2',
+            tags: [
+              { id: 1, name: 'tag1' },
+              { id: 2, name: 'tag2' },
+            ],
+            created_at: '2021-01-13 14:59:41',
+          },
+        ])
+      )
+    }
+  ),
+]
